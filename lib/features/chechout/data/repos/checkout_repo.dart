687d238474036e33fa955d1f7991b81660045119
Mina Kamil <mena.m4077@gmail.com>
2026-06-12 03:12:56 +0300

    remove old code

--- lib/features/chechout/data/repos/checkout_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:checkout_payment_by_paymab/core/errors/failures.dart';
-import 'package:checkout_payment_by_paymab/features/chechout/data/models/payment_intent_input_model.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class CheckoutRepo {
-  Future<Either<Failure, void>> makePayment(
-      {required PaymentIntentInputModel paymentIntentInputModel});
-}
