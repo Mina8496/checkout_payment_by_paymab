54838efc2bed7bebba816d9d8deeb2a30712def0
Mina Kamil <mena.m4077@gmail.com>
2026-06-12 03:14:30 +0300

    feat(Dio helper): add widget service dio helper

---
+++ lib/core/utils/constant/api_key.dart
@@ -0,0 +1,9 @@
+
+   const String paymobApiKey =
+      'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRFMk9URTRPQ3dpYm1GdFpTSTZJbWx1YVhScFlXd2lmUS5OcjhIYlUzMWVEUGxLd2NrLVE1M3kzN0k3SU1JSFM4X3YxY2dLa1kxSkhkRWowci16THFObmo3UmFlQWx4bHJNNXJFdk0zeGk2a2VXSVJRcmxfVXRGQQ==';
+   const String authToken = "";
+   const String orderId = "";
+   const double integrationCardID = 5691510;
+   const String requestTokenCard = "";
+
+
