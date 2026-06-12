326f80bb041aaef28964f35da6eefe2da6603732
Mina Kamil <mena.m4077@gmail.com>
2026-06-12 03:18:24 +0300

    feat(MyBlocObserver): add MyBlocObserver refactor payment state

--- lib/features/chechout/presentation/manger/cubit/payment_state.dart
+++ lib/features/chechout/presentation/manger/cubit/payment_state.dart
@@ -12,24 +12,4 @@ final class PaymentErrorAuthToken extends PaymentState {
   PaymentErrorAuthToken(this.errMessage);
 }
 
-final class LoadingOrderID extends PaymentState {}
-
-final class SuccessOrderID extends PaymentState {}
-
-final class ErrorOrderID extends PaymentState {
-  final String errMessage;
-
-  ErrorOrderID(this.errMessage);
-}
-
-final class LoadingRquestTokenPaymentState extends PaymentState {}
-
-final class SuccessRquestTokenPaymentState extends PaymentState {}
-
-final class ErrorRquestTokenPaymentState extends PaymentState {
-  final String errMessage;
-
-  ErrorRquestTokenPaymentState(this.errMessage);
-}
-
 
